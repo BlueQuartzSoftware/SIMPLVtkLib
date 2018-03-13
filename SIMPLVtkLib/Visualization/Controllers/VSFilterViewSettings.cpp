884498e40a09338712621bf7b22397f18bdc41e6
Joey Kleingers <joey.kleingers@gmail.com>
2018-03-13 14:11:47 -0400

    Removing warning.

--- SIMPLVtkLib/Visualization/Controllers/VSFilterViewSettings.cpp
+++ SIMPLVtkLib/Visualization/Controllers/VSFilterViewSettings.cpp
@@ -767,7 +767,6 @@ void VSFilterViewSettings::setupDataSetActors()
     }
     else
     {
-      int currentComponent = m_ActiveComponent;
       setActiveArrayIndex(m_ActiveArray);
       setActiveComponentIndex(m_ActiveComponent);
     }
